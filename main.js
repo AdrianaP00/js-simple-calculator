b4e5a8634b2117a0f2efb3deca3f88666db2af69
Adriana Piccolo <134702809+AdrianaP00@users.noreply.github.com>
2023-06-15 12:23:08 +0000

    add function

--- main.js
+++ main.js
@@ -4,27 +4,32 @@ const operator = prompt('scegli un operazione (either+,-,*,/):');
 const num1 = parseFloat (prompt('scegli un numero'));
 const num2 = parseFloat (prompt(' scegli un altro numero'));
 
-switch (operator){
-    case '+':
-        result = num1 + num2;
-        console.log (`${num1} + ${num2} = ${result}`);
-        break;
-    
-    case '-':
-        result = num1 - num2;
-        console.log (`${num1} - ${num2} = ${result}`);
-        break;
-    
-    case '*':
-        result = num1 * num2;
-        console.log (`${num1} * ${num2} = ${result}`);
-        break;
-     
-    case '/':
-        result = num1 / num2;
-        console.log (`${num1} / ${num2} = ${result}`)
-    
-    default:
-         console.log('invalid operator')
-         break;
+inputMe(operator,num1, num2)
+
+function inputMe(operator, num1,num2) {
+
+    switch (operator){
+        case '+':
+            result = num1 + num2;
+            console.log (`${num1} + ${num2} = ${result}`);
+            break;
+        
+        case '-':
+            result = num1 - num2;
+            console.log (`${num1} - ${num2} = ${result}`);
+            break;
+        
+        case '*':
+            result = num1 * num2;
+            console.log (`${num1} * ${num2} = ${result}`);
+            break;
+        
+        case '/':
+            result = num1 / num2;
+            console.log (`${num1} / ${num2} = ${result}`)
+        
+        default:
+            console.log('invalid operator')
+            break;
+    }
 }
