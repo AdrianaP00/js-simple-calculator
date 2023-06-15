1be82d95b2f0e18e9b8be17d8094db70680279e8
Adriana Piccolo <134702809+AdrianaP00@users.noreply.github.com>
2023-06-15 23:09:46 +0000

    code refactor

--- main.js
+++ main.js
@@ -2,63 +2,58 @@ const prompt = require('prompt-sync')();
 
 let menuCal = 0;
 do {
-    console.log (`1 - start`);
-    console.log (`2 - stop`);
-    console.log (`Hit 'x' to exit`); 
-    menuCal= prompt(``);
+    console.log(`--------------------`);
+    console.log(`| 1 - start        |`);
+    console.log(`| 2 - stop         |`);
+    console.log(`| Hit 'x' to exit  |`);
+    console.log(`====================\n`);
+    menuCal = prompt(``);
 
     switch (menuCal) {
-        case 'x': out();
-            
+        case 'x':
+        case '2': stop();
             break;
-            
-            case '2': stop();
-            
+        case '1': calculate();
             break;
-    
         default:
             break;
     }
-} while (menuCal != '1');
+} while (menuCal != 'x' && menuCal != '2');
 
-function stop(){
-    console.log('♥');
+function calculate() {
+    const operator = prompt('\nscegli un operazione (either+,-,*,/): ');
+    const num1 = parseFloat(prompt('scegli un numero: '));
+    const num2 = parseFloat(prompt('scegli un altro numero: '));
+
+    inputMe(operator, num1, num2);
 }
 
-function out(){
-    console.log('See you soon');
-}
+function inputMe(operator, num1, num2) {
 
-const operator = prompt('scegli un operazione (either+,-,*,/):');
-const num1 = parseFloat (prompt('scegli un numero'));
-const num2 = parseFloat (prompt(' scegli un altro numero'));
-
-inputMe(operator,num1, num2);
-
-function inputMe(operator, num1,num2) {
-    
-    switch (operator){
+    switch (operator) {
         case '+':
             result = num1 + num2;
-            console.log (`${num1} + ${num2} = ${result}`);
             break;
-        
+
         case '-':
             result = num1 - num2;
-            console.log (`${num1} - ${num2} = ${result}`);
             break;
-        
+
         case '*':
             result = num1 * num2;
-            console.log (`${num1} * ${num2} = ${result}`);
             break;
-        
+
         case '/':
             result = num1 / num2;
-            console.log (`${num1} / ${num2} = ${result}`)
-        
+
         default:
             console.log('invalid operator')
             break;
     }
+
+    console.log(`\n${num1} ${operator} ${num2} = ${result}`);
+}
+
+function stop() {
+    console.log('♥');
 }
