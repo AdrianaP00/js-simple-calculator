cbd43d6352d9e8e1ce915b38c7fa6b27170c97c0
Adriana Piccolo <134702809+AdrianaP00@users.noreply.github.com>
2023-06-15 12:53:23 +0000

    add menu

--- main.js
+++ main.js
@@ -1,13 +1,42 @@
 const prompt = require('prompt-sync')();
 
+let menuCal = 0;
+do {
+    console.log (`1 - start`);
+    console.log (`2 - stop`);
+    console.log (`Hit 'x' to exit`); 
+    menuCal= prompt(``);
+
+    switch (menuCal) {
+        case 'x': out();
+            
+            break;
+            
+            case '2': stop();
+            
+            break;
+    
+        default:
+            break;
+    }
+} while (menuCal != '1');
+
+function stop(){
+    console.log('♥');
+}
+
+function out(){
+    console.log('See you soon');
+}
+
 const operator = prompt('scegli un operazione (either+,-,*,/):');
 const num1 = parseFloat (prompt('scegli un numero'));
 const num2 = parseFloat (prompt(' scegli un altro numero'));
 
-inputMe(operator,num1, num2)
+inputMe(operator,num1, num2);
 
 function inputMe(operator, num1,num2) {
-
+    
     switch (operator){
         case '+':
             result = num1 + num2;
